01ed6052c8f2402542a4fcedeab92420361e2610
thatvirtualboy <thatvirtualboy@icloud.com>
2015-09-04 10:28:19 -0600

    Added FTP functionality

--- HorizonCollector.sh
+++ HorizonCollector.sh
@@ -5,16 +5,34 @@
 #
 
 echo "Collecting Horizon View Logs..."
-sleep 4s
+sleep 2s
 echo "WARNING: you must run this as sudoer if you need to gather USB related logs"
-sleep 4s
+sleep 2s
 
-zip -r9 ~/Desktop/vmware-logs-`date +%Y-%m-%d_%I.%M.%S_%p_%Z`.zip ~/Library/Logs/VMware* ~/Library/Preferences/ByHost/com.microsoft.rdc.*.plist ~/Library/Preferences/com.microsoft.rdc.plist /Library/Logs/VMware* --exclude=*fusion -x=*Fusion* &> /dev/null
+zipfile=vmware-logs-`date +%Y-%m-%d_%I.%M.%S_%p_%Z`.zip
 
+zip -r9 ~/Desktop/$zipfile ~/Library/Logs/VMware* ~/Library/Preferences/ByHost/com.microsoft.rdc.*.plist ~/Library/Preferences/com.microsoft.rdc.plist /Library/Logs/VMware* --exclude=*fusion -x=*Fusion* &> /dev/null
+
+echo "Upload logs to existing VMware Support Request?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) read -p "Please type the SR Number and press [ENTER]:" SR; 
+			  echo "Uploading..." 
+              curl -#T $zipfile --ftp-create-dirs ftp://ftpsite.vmware.com/$SR/`basename $zipfile` --user inbound:inbound
+              echo "All done! The bundle is also saved to your desktop: $zipfile"
+				break;;
+        No ) 
 echo "***************************************************"
 echo "***************************************************"
 echo "                                                   "
 echo "    All done! The log bundle is on your desktop    "
 echo "                                                   "
 echo "***************************************************"
-echo "***************************************************"
+echo "***************************************************"; exit;;
+    esac
+done
+
+
+
+
+
